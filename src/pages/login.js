fdffbfeedad5a532c5de851f5e9c325658108209
nychavs <chavesnaiury@gmail.com>
2023-06-18 17:58:11 -0300

    login done

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,36 @@
 import React from "react";
-
+import login from "../styles/login.css"
+import Navbarhome from "../components/navbarhome"
 const Login = () => {
   return (
     <div>
-      <h1>Login page</h1>
-    </div>
+      <Navbarhome/>
+      <div class="login-content">
+        <div class="login-box">
+      <form>
+      <h1>Bem vindo de volta!</h1>
+        <label>Nome de usuário ou email</label>
+            <input 
+            type='text' 
+            id='usuario' 
+            name='usuario' 
+            /><br/>
+            <label>Senha</label>
+            <input
+            type='password' 
+            id='senhaUsuario' 
+            name='senhaUsuario' 
+            /><br/>
+            <button 
+            type="submit" 
+            value="Submit" 
+            > 
+            Prosseguir
+            </button>
+          </form>
+          </div>
+        </div>
+      </div>
   );
 };
 
