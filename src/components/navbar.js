f7c352f81ca869f5cf72f238c29d8ec91eec9316
nychavs <chavesnaiury@gmail.com>
2023-06-20 10:11:43 -0300

    going back to home by logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,14 @@ import logo from "../assets/logo.png";
 import navbarhome from "../styles/navbarhome.css";
 
 const Navbar = () => {
+  
+    function goHome(){
+      window.location.href = "http://localhost:3000/home";
+  }
+
   return (
     <div class="content">
-    <img width={"115vw"} height={"65vh"} src={logo}></img>
+    <img width={"115vw"} height={"65vh"} src={logo} onClick={goHome}></img>
       <div class="options">
         <Link to="/">Sobre nós</Link>
         <Link to="/">Ajuda</Link>
