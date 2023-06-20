3538a159225b8a4d4a8849d860a688189d02530a
nychavs <chavesnaiury@gmail.com>
2023-06-20 10:11:04 -0300

    image finished

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,46 @@
 import React from "react";
-import home from "../styles/home.css"
+import home from "../styles/home.css";
 import logo from "../assets/logo.png";
+import Navbar from "../components/navbar.js"
+import rumine from '../assets/rumine.jpg'
+import cardigan from "../assets/taylor-swift-cardigan2.jpg"
+import { Link } from "react-router-dom";
+
 
 const musicas = [
-  {name: "cardigan",
-  singer: "taylor"},
-  {name: "r u mine",
-  singer: "arctic monkeys"}]; 
+  { name: "cardigan", singer: "taylor", img:cardigan, video:"https://www.youtube.com/embed/K-a8s8OLBSE" },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine, video:"https://www.youtube.com/embed/ngzC_8zqInk" },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine },
+  { name: "r u mine", singer: "arctic monkeys", img:rumine }
+];
 
 const Home = () => {
-  <>
-  <h1>teste</h1>
-  {musicas.map((item, index) => (
-    <div key={index}>
+  return (
+    <div>
+      <Navbar/>
       <div class="home-container">
-      <img width={"115vw"} height={"65vh"} src={logo}></img>
-          <p>Name: {item.name}</p>
-          <p>Singer: {item.singer}</p>
-        </div>
+        <div class="square-container">
+        {musicas.map((item,index) => (
+          <div class="square" key={index}>
+             <Link to={`/detail/${item.name}/${item.singer}/${encodeURIComponent(item.video)}`}>
+                <img width={"106vw"} height={"80vh"} src={item.img} alt="Imagem" />
+              </Link>
+              <p>{item.name}</p>
+              <p>{item.singer}</p>
+            </div>
+        ))}
+      </ div>
+    </div>
       </div>
-      ))}
   );
-  </>
 };
 
 export default Home;
