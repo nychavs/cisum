309dd0e6927e51bbee443bd99ecdf2671352bfb4
nychavs <chavesnaiury@gmail.com>
2023-06-20 10:11:29 -0300

    detalhes with youtube video

--- src/pages/detalhes.js
+++ src/pages/detalhes.js
@@ -1,8 +1,29 @@
 import React from "react"
+import { useParams } from "react-router-dom";
+import detalhes from "../styles/detalhes.css"
+import Navbar from "../components/navbar";
 
 const Detalhes = () => {
-    return(
-        <h1>teste</h1>
-    )
-}
-export default Detalhes;
+    const { nome, singer, video } = useParams();
+  
+    return (
+      <div>
+        <Navbar/>
+        <div class="detalhes-container">
+        <div class="video">
+            <iframe width="699" 
+            height="393" 
+            src={video} 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowfullscreen>
+            </iframe>
+        </div>
+        <h1>{nome}</h1>
+        <h1>{singer}</h1>
+        </div>
+      </div>
+    );
+  };
+export default Detalhes;  
+  
