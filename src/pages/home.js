388d442b86281076aa15d5b54086655d6f6a6d0d
nychavs <chavesnaiury@gmail.com>
2023-06-20 07:43:13 -0300

    trying to map

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,27 @@
 import React from "react";
 import home from "../styles/home.css"
+import logo from "../assets/logo.png";
+
+const musicas = [
+  {name: "cardigan",
+  singer: "taylor"},
+  {name: "r u mine",
+  singer: "arctic monkeys"}]; 
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+  <>
+  <h1>teste</h1>
+  {musicas.map((item, index) => (
+    <div key={index}>
+      <div class="home-container">
+      <img width={"115vw"} height={"65vh"} src={logo}></img>
+          <p>Name: {item.name}</p>
+          <p>Singer: {item.singer}</p>
+        </div>
+      </div>
+      ))}
   );
+  </>
 };
 
 export default Home;
